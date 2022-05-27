6dac57d0ddd6e254e4451ea1b74a895d37972cc4
szejiancheng <sze2001jc@gmail.com>
2022-05-27 15:00:03 +0800

    Feature - Sign in auth with firebase
    Added navigable main menu, Authmanager and UI manager for quality of (our) lives

---
+++ PASGIG/Assets/Scripts/CloudScript.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudScript : MonoBehaviour
+{
+    private float _speed = 2;
+    private float _endPosX;
+ 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void StartFloating(float speed, float endPosX)
+    {
+        _speed = speed;
+        _endPosX = endPosX;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.right * Time.deltaTime * _speed);
+
+        if (transform.position.x > _endPosX) {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ PASGIG/Assets/Scripts/UIManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance;
+
+    //Screen object variables
+    public GameObject loginUI;
+    public GameObject registerUI;
+    public GameObject IntroUI;
+
+    public TMP_Text warningLoginText;
+    public TMP_Text confirmLoginText;
+    public TMP_Text warningRegisterText;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != null)
+        {
+            Debug.Log("Instance already exists, destroying object!");
+            Destroy(this);
+        }
+    }
+
+    //Functions to change the login screen UI
+    public void LoginScreen() //Back button
+    {
+        loginUI.SetActive(true);
+        ClearAllText();
+        registerUI.SetActive(false);
+        
+    }
+    public void RegisterScreen() // Register button
+    {
+        loginUI.SetActive(false);
+        ClearAllText();
+        registerUI.SetActive(true);
+    }
+    public void IntroScreen()
+    {
+        loginUI.SetActive(false);
+        ClearAllText();
+        IntroUI.SetActive(true);
+    }
+
+    public void ClearAllText()
+    {
+        warningLoginText.text = "";
+        warningRegisterText.text = "";
+        confirmLoginText.text = "";
+    }
+}
