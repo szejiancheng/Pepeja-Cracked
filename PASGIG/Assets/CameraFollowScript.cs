f45b2d69efa5a38c76309cc4d2278c78ae304db7
szejiancheng <sze2001jc@gmail.com>
2022-06-26 01:45:16 +0800

    Fix - Followcamera fixes
    what it says on the tin I have covid I'm real tired

--- PASGIG/Assets/CameraFollowScript.cs
+++ PASGIG/Assets/CameraFollowScript.cs
@@ -4,39 +4,44 @@ using UnityEngine;
 
 public class CameraFollowScript : MonoBehaviour
 {
-    //TODO: Make dynamic camera resize to include objects of interest
     //TODO: optimize
 
     //https://www.youtube.com/watch?v=aLpixrPvlB8&ab_channel=Brackeys ^^ inspired
 
-    public List<Transform> targets;
+    //public List<Transform> targets;
     public Vector2 offset;
-    public float smoothTime = 0.5f;
+    public float smoothTime;
     private Vector2 velocity;
     public float minZoom = 50f;
     public float maxZoom = 20f;
     public float zoomLimiter = 30f;
+    public float detectionRadius = 60f;
+    public Collider2D player;
 
 
-    //TODO: make use of Physics.Overlap to create a dynamic array to render
-    Collider[] PointsOfInterest;
+    public List<Collider2D> targets = new List<Collider2D>();
+    ContactFilter2D filter = new ContactFilter2D();
 
     private Camera cam;
 
     void Start()
     {
         cam = GetComponent<Camera>();
+        targets.Add(player);
     }
+
     
-    /*
     private void Update()
     {
-        Physics.OverlapCircleNonAlloc(targets[0].position, 50f, PointsOfInterest);
-        for(int i = 0; i < PointsOfInterest.Length; i++) {
-            Debug.Log(PointsOfInterest[i]);
-        }
+        Physics2D.OverlapCircle(player.transform.position, detectionRadius, filter, targets);
+        //foreach (Collider2D collider in targets) {
+        //    Debug.Log(collider);
+        //}
+        targets.Add(player);
     }
-    */
+    
+    
+
 
     void LateUpdate() 
     {
@@ -46,44 +51,44 @@ public class CameraFollowScript : MonoBehaviour
         }
         Move();
         Zoom();
-        
     }
 
 
     void Move() 
     {
         Vector2 centerPoint = GetCenterPoint();
-        Vector2 newPosition = centerPoint + offset;
-        //transform.position = Vector2.SmoothDamp(transform.position, newPosition, ref velocity, smoothTime);
-        transform.position = newPosition;
+        //Vector2 newPosition = centerPoint + offset;
+        transform.position = Vector2.MoveTowards(transform.position, centerPoint, smoothTime);
+        //transform.position = centerPoint;
     }
 
     void Zoom()
     {
         float newZoom = Mathf.Lerp(maxZoom, minZoom, GetGreatestDist()/zoomLimiter);
-        cam.orthographicSize = newZoom;
+        cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, newZoom, Time.deltaTime);
+        //cam.orthographicSize = 30;
     }
 
     float GetGreatestDist()
     {
-        var bounds = new Bounds(targets[0].position, Vector2.zero);
+        var bounds = new Bounds(player.transform.position, Vector2.zero);
         for (int i = 0; i < targets.Count; i++)
         {
-            bounds.Encapsulate(targets[i].position);
+            bounds.Encapsulate(targets[i].transform.position);
         }
-        return bounds.size.x;
+        return bounds.size.magnitude;
     }
     Vector2 GetCenterPoint()
     {
         if (targets.Count == 1)
         {
-            return targets[0].position;
+            return targets[0].transform.position;
         }
 
-        var bounds = new Bounds(targets[0].position, Vector2.zero);
+        var bounds = new Bounds(player.transform.position, Vector2.zero);
         for (int i = 0; i < targets.Count; i++)
         {
-            bounds.Encapsulate(targets[i].position);
+            bounds.Encapsulate(targets[i].transform.position);
         }
         return bounds.center;
     }
